910f395fd8378c026db260548a67f10ecd7a0e66
EmmaTBxx <maneyras@gmail.com>
2023-06-27 04:10:56 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,21 +7,19 @@
  */
 void print_rev(char *s)
 {
-	int i, j, len;
+	int c = 0;
 
 	i = 0;
 
-	while (s[i] != '\0')
+	while (s[c] != '\0')
 	{
-		i++;
+		c++;
 	}
 
-	len = i;
-
-	for (j = len - 1; j  >= 0; j--)
+	for (c -= 1; c  >= 0; c--)
 	{
-		_putchar(s[j]);
+		_putchar(s[c]);
 	}
 
-	_putchar('n');
+	_putchar('\n');
 }
